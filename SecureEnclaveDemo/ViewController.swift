4f8f68557891f0261b8e5087957948fe53e69645
Ali Murad <rxalimurad@gmail.com>
2024-05-11 12:38:14 +0500

    Initial Commit

---
+++ SecureEnclaveDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecureEnclaveDemo
+//
+//  Created by Ali Murad on 11/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
